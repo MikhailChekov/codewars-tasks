97db92a4133b9700cdbdd4fe1eb5b7e687a22a76
Mikhail Chekov <chekoff@list.ru>
2020-09-07 11:14:22 +0300

    ball task refactoring

--- The ball bounces/index.js
+++ The ball bounces/index.js
@@ -1,25 +1,19 @@
 function bouncingBall(h,  bounce,  window) {
-    let downHeight = h;
-    let upHeight = downHeight;
     let steps = 0;
     
     //check for error conditions before begin
     if (h < 0 || bounce < 0|| bounce > 1 || window > h) return -1;
 
     // 1 floor step
-    while(downHeight > window){
+    while(h > window){
         steps++;
-        upHeight *= bounce;
-        if(upHeight > window){
-            steps++;
-            downHeight = upHeight;
-        }else{
-            break;
-        }
+        h *= bounce;
+        if(h > window) steps++;
     }
 
     //condition   -   if < 1 return -1 
     if(steps == 0) return -1;
   
+
     return steps;
 }
