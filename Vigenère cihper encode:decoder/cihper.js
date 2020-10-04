15cc7871fcbebfeb83e97c3a35b65bb51e66a6f7
Mikhail Chekov <chekoff@list.ru>
2020-10-04 15:31:21 +0300

    Task almost ready, some test not working

---
+++ Vigenère cihper encode:decoder/cihper.js
@@ -0,0 +1,82 @@
+// almost working, some test with '\' not correct
+
+function VigenèreCipher(key, abc) {
+  (this.abc = abc || 'abcdefghijklmnopqrstuvwxyz'),
+    (this.key = key || 'password');
+
+  this.checkWordLength = function (str) {
+    let newKey = '';
+    if (str.length > this.key.length) {
+      let rest = str.length - this.key.length;
+      let quotient = Math.floor(rest / this.key.length);
+      let remainder = rest % this.key.length;
+      if (quotient > 1) {
+        let chunk = this.key.repeat(quotient);
+        newKey += chunk;
+      }
+      if (remainder) {
+        newKey = this.key;
+        for (let i = 0; i < remainder; i++) {
+          newKey += this.key[i];
+        }
+      }
+    } else {
+      newKey = this.key;
+    }
+
+    return newKey;
+  };
+
+  this.getIndex = function (str) {
+    let indContainer = [];
+    let abc = this.abc.split('');
+    let arr = str.split('');
+    arr.forEach((e) => {
+      !this.abc.includes(e) ? indContainer.push(e) : indContainer.push(this.abc.indexOf(e, 0));
+    });
+    return indContainer;
+  };
+
+  this.encode = function (str) {
+    let res = [];
+    let newKey = this.checkWordLength(str);
+    let keyInd = this.getIndex(newKey);
+    let wordInd = this.getIndex(str);
+
+    wordInd.forEach((e, i) => {
+      if (!Number.isInteger(e)) {
+        res.push(e);
+      } else {
+        
+        let encInd = e + keyInd[i];
+        if (encInd > 25) {
+          encInd -= 26;
+        }
+        let encSym = this.abc[encInd];
+        res.push(encSym);
+      }
+    });
+    return res.join('');
+  };
+  this.decode = function (str) {
+    let res = [];
+    let newKey = this.checkWordLength(str);
+    let keyInd = this.getIndex(newKey);
+    let wordInd = this.getIndex(str);
+    wordInd.forEach((e, i) => {
+      if (!Number.isInteger(e)) {
+        res.push(e);
+      } else {
+        let decInd = e - keyInd[i];
+        if (decInd < 0) {
+          decInd += 26;
+        }
+        let decSym = this.abc[decInd];
+        res.push(decSym);
+      }
+    });
+    return res.join('');
+  };
+}
+let c = new VigenèreCipher();
+console.log(c.decode('xt\'k o vwixl qzswej!'));
--- Vigenère cihper encode:decoder/index.js
+++ Vigenère cihper encode:decoder/index.js
@@ -1,39 +1,83 @@
-// TODO: decode method
+// almost working, some test with '\' not correct
 
-function VigenèreCipher() {
-    let keyInd = [], wordInd = [], encode = [], decode = [];
-    abc = abc.split('');
-    key = key.split('');
-    word = word.split('');
-    encWord = encWord.split('');
-
-    key.forEach(e => {
-        keyInd.push(abc.indexOf(e, 0));
-    });
-    word.forEach(e => {
-        wordInd.push(abc.indexOf(e, 0));
-    });
-    console.log(wordInd, 'wordInd');
-    console.log(keyInd, 'keyInd');
-
-    wordInd.forEach((e , i)=> {
-        let encInd = e + keyInd[i];
-        if(encInd > 25){
-            encInd -=26;
+function VigenèreCipher(key, abc) {
+    (this.abc = abc || 'abcdefghijklmnopqrstuvwxyz'),
+      (this.key = key || 'password');
+  
+    this.checkWordLength = function (str) {
+      let newKey = '';
+      if (str.length > this.key.length) {
+        let rest = str.length - this.key.length;
+        let quotient = Math.floor(rest / this.key.length);
+        let remainder = rest % this.key.length;
+        if (quotient > 1) {
+          let chunk = this.key.repeat(quotient);
+          newKey += chunk;
         }
-        let encSym = abc[encInd];
-        encode.push(encSym);
-    });
-    
-    console.log(encode);
-
-
-    // this.encode = function (str) 
-    // this.decode = function (str) 
-}
-
-let key = 'password', abc = 'abcdefghijklmnopqrstuvwxyz' , word = 'codewars', encWord = 'rovwsoiv';
-
-VigenèreCipher(key, abc, word, encWord);
-
-    // 'codewars', 'rovwsoiv'
+        if (remainder) {
+          newKey = this.key;
+          for (let i = 0; i < remainder; i++) {
+            newKey += this.key[i];
+          }
+        }
+      } else {
+        newKey = this.key;
+      }
+  
+      return newKey;
+    };
+  
+    this.getIndex = function (str) {
+      let indContainer = [];
+      let abc = this.abc.split('');
+      let arr = str.split('');
+      arr.forEach((e) => {
+        !this.abc.includes(e) ? indContainer.push(e) : indContainer.push(this.abc.indexOf(e, 0));
+      });
+      return indContainer;
+    };
+  
+    this.encode = function (str) {
+      let res = [];
+      let newKey = this.checkWordLength(str);
+      let keyInd = this.getIndex(newKey);
+      let wordInd = this.getIndex(str);
+  
+      wordInd.forEach((e, i) => {
+        if (!Number.isInteger(e)) {
+          res.push(e);
+        } else {
+          
+          let encInd = e + keyInd[i];
+          if (encInd > 25) {
+            encInd -= 26;
+          }
+          let encSym = this.abc[encInd];
+          res.push(encSym);
+        }
+      });
+      return res.join('');
+    };
+    this.decode = function (str) {
+      let res = [];
+      let newKey = this.checkWordLength(str);
+      let keyInd = this.getIndex(newKey);
+      let wordInd = this.getIndex(str);
+      wordInd.forEach((e, i) => {
+        if (!Number.isInteger(e)) {
+          res.push(e);
+        } else {
+          let decInd = e - keyInd[i];
+          if (decInd < 0) {
+            decInd += 26;
+          }
+          let decSym = this.abc[decInd];
+          res.push(decSym);
+        }
+      });
+      return res.join('');
+    };
+  }
+//   let c = new VigenèreCipher();
+  
+  
